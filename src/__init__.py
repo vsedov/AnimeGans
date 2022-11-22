ae536e0339b595a1c3410cab17ef15a309b20bfc
vsedov <viv.sedov@hotmail.com>
2022-11-22 22:50:14 +0000

    feat(core.py): with infereince on init and main.py

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+from src.utils.core_helper import setup_constants, setup_extra_constants, setup_globals
+
+#  ╭────────────────────────────────────────────────────────────────────╮
+#  │             Set up all global constants using hc class             │
+#  │                                                                    │
+#  ╰────────────────────────────────────────────────────────────────────╯
+setup_constants()
+setup_extra_constants()
+setup_globals()
