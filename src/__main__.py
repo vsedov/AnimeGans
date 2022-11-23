2d633edd08067e381747fc8bd5f0f70ad1d066d1
vsedov <viv.sedov@hotmail.com>
2022-11-23 00:07:43 +0000

    chore(formatting/cleanup): Constants / misc __main__

--- src/__main__.py
+++ src/__main__.py
@@ -1,1 +1,12 @@
-# Here we can call or evaluate what ever function we want to work with
+from src.core import hc
+
+#  TODO: (vsedov) : Refactor this
+if hc.initial["gan"]:
+    from src.initial.gans.runner import setup
+
+    setup()
+
+if hc.initial["conv"]:
+    from src.initial.image_proc.kernel_understanding import setup
+
+    setup()
