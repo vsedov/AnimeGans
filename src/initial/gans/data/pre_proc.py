f4ccf6d7ca98835de9d3d28afc728f1ee7959840
vsedov <viv.sedov@hotmail.com>
2022-11-25 01:39:43 +0000

    fix(pre_proc): Folder fix, Creates folder if they dont exist , move data if required and now have an option to split between the cat and dog dataset using index.

--- src/initial/gans/data/pre_proc.py
+++ src/initial/gans/data/pre_proc.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 import matplotlib.animation as animation
 import numpy as np
@@ -13,8 +14,8 @@ from src.core import hc
 
 #  TODO: (vsedov) (01:28:44 - 24/11/22): Make this into a dataclass - for core refactoring
 path = os.path.dirname(os.path.abspath(__file__))
-train_path = f"{os.path.dirname(path)}/animal/"
-test_path = f"{os.path.dirname(path)}/animal/test1/"
+train_path = f"{os.path.dirname(path)}/data/animal/"
+test_path = f"{os.path.dirname(path)}/data/animal/test1/"
 animal_types = f"{path}/animal/types/"
 image_size = 64
 
@@ -25,7 +26,25 @@ class Data(data.Dataset):
         Get Image Path.
         """
         self.root = root
-        self.folder = os.path.join(root, "train")
+        self.folder = f"{self.root}train"
+
+    def move_data_to_types(self):
+        # !mv train/dog.*.jpg types/dog/
+        # !mv train/cat.*.jpg types/cat/
+        # if dog or cat does not exist, create it
+        if not os.path.exists(f"{animal_types}/dog"):
+            os.makedirs(f"{animal_types}/dog")
+        if not os.path.exists(f"{animal_types}/cat"):
+            os.makedirs(f"{animal_types}/cat")
+
+        with os.scandir(self.folder) as entries:
+            for entry in entries:
+                if entry.name.startswith("dog"):
+                    shutil.move(entry.path, f"{animal_types}/dog/")
+                elif entry.name.startswith("cat"):
+                    shutil.move(entry.path, f"{animal_types}/cat/")
+                else:
+                    print("No match")
 
     def create_data(self):
         self.dataset = torchvision.datasets.ImageFolder(
@@ -39,9 +58,18 @@ class Data(data.Dataset):
                 ]
             ),
         )
+        dog_index = self.dataset.class_to_idx["dog"]
+        cat_index = self.dataset.class_to_idx["cat"]
+        self.dog_dataset = torch.utils.data.Subset(
+            self.dataset, [i for i, t in enumerate(self.dataset.targets) if t == dog_index]
+        )
+        self.cat_dataset = torch.utils.data.Subset(
+            self.dataset, [i for i, t in enumerate(self.dataset.targets) if t == cat_index]
+        )
 
-    def get_dl(self, batch_size=32, shuffle=True, drop_last=True):
-        return torch.utils.data.DataLoader(self.dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last)
+    def get_dl(self, batch_size=32, shuffle=True, drop_last=True, dog=True):
+        data_set_type = self.dog_dataset if dog else self.cat_dataset
+        return torch.utils.data.DataLoader(data_set_type, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last)
 
     def view_training_data(self):
         """
@@ -50,7 +78,7 @@ class Data(data.Dataset):
         Utalises Matplotlib to show the images and its animation features.
         """
         # Plot some training images
-        real_batch = next(iter(self.dataloader))
+        real_batch = next(iter(self.get_dl()))
 
         plt.figure(figsize=(8, 8))
         plt.axis("off")
@@ -65,7 +93,7 @@ class Data(data.Dataset):
         fig = plt.figure()
         ims = []
         for i in range(10):
-            real_batch = next(iter(self.dataloader))
+            real_batch = next(iter(self.get_dl()))
             ims.append(
                 [
                     plt.imshow(
