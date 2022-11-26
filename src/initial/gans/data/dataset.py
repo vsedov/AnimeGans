ea970a579ff9c680223c1aa64dcb7e30c0205d8b
vsedov <viv.sedov@hotmail.com>
2022-11-26 04:28:44 +0000

    Feat(pre data proc): Refactor Data class, and add human face ds to test on. The Data Class, will be only for the cats and dog dataset and the human face dataset, as this was the basis for the animé ds that i will be working with later down the line.

--- src/initial/gans/data/dataset.py
+++ src/initial/gans/data/dataset.py
@@ -6,6 +6,17 @@ from src.data.getdata import DSDownloader
 
 
 def set_up_dog_ds():
+    """
+    Setup Cat and dog ds:
+    We split the ds, into two paths and extract any extended zip files required.
+    This is done automatically.
+
+    Zip path: is the core directory of where the data should go.
+
+    Unzip file: Animal ds path extraction of the initial ds
+    command: Command to call from kaggle
+    name: the extracted zip file that we will get.
+    """
     zip_path = f"{hc.DIR}initial/gans/data"
     unzip_path = f"{hc.DIR}initial/gans/data/animal"
     command = f"kaggle competitions download -c dogs-vs-cats -p {zip_path} --force"
@@ -20,3 +31,18 @@ def set_up_dog_ds():
 
         with zipfile.ZipFile(f"{unzip_path}/{val}") as unzip:
             unzip.extractall(unzip_path)
+
+
+def set_up_face_ds():
+    """
+    Setup Face Dataset:
+
+    No external extraction will be required.
+
+    Quite a large dataset.
+    """
+    zip_path = f"{hc.DIR}initial/gans/data"
+    unzip_path = f"{hc.DIR}/initial/gans/data/human_faces/"
+    command = f"kaggle datasets download -d jessicali9530/celeba-dataset -p {zip_path} --force"
+    name = "celeba-dataset.zip"
+    DSDownloader(command, zip_path, unzip_path, name)()
--- src/initial/gans/data/pre_proc.py
+++ src/initial/gans/data/pre_proc.py
@@ -12,73 +12,81 @@ from torchvision import transforms
 
 from src.core import hc
 
-#  TODO: (vsedov) (01:28:44 - 24/11/22): Make this into a dataclass - for core refactoring
-path = os.path.dirname(os.path.abspath(__file__))
-train_path = f"{os.path.dirname(path)}/data/animal/"
-test_path = f"{os.path.dirname(path)}/data/animal/test1/"
-animal_types = f"{path}/animal/types/"
-image_size = 64
-
 
 class Data(data.Dataset):
-    def __init__(self, root=train_path):
-        """
-        Get Image Path.
-        """
-        self.root = root
-        self.folder = f"{self.root}train"
+    def __init__(self):
+        self.path = os.path.dirname(os.path.abspath(__file__))
+        self.dirname = os.path.dirname(self.path)
+        self.ds_path = None
+        self.animal = False
+
+        self.image_size = hc.initial["params"]["image_size"]
+
+    def set_animal_path(self):
+        self.ds_path = f"{self.path}/animal/types/"
+        self.animal = True
+        print(self.ds_path)
+
+    def set_human_path(self):
+        self.ds_path = f"{self.path}/human_faces/img_align_celeba/"
+        print(self.ds_path)
 
     def move_data_to_types(self):
         # !mv train/dog.*.jpg types/dog/
         # !mv train/cat.*.jpg types/cat/
         # if dog or cat does not exist, create it
-        if not os.path.exists(f"{animal_types}/dog"):
-            os.makedirs(f"{animal_types}/dog")
-        if not os.path.exists(f"{animal_types}/cat"):
-            os.makedirs(f"{animal_types}/cat")
+        if not os.path.exists(f"{self.ds_path}/dog"):
+            os.makedirs(f"{self.ds_path}/dog")
+        if not os.path.exists(f"{self.ds_path}/cat"):
+            os.makedirs(f"{self.ds_path}/cat")
 
         with os.scandir(self.folder) as entries:
             for entry in entries:
                 if entry.name.startswith("dog"):
-                    shutil.move(entry.path, f"{animal_types}/dog/")
+                    shutil.move(entry.path, f"{self.ds_path}/dog/")
                 elif entry.name.startswith("cat"):
-                    shutil.move(entry.path, f"{animal_types}/cat/")
+                    shutil.move(entry.path, f"{self.ds_path}/cat/")
                 else:
                     print("No match")
 
     def create_data(self):
         self.dataset = torchvision.datasets.ImageFolder(
-            root=animal_types,
+            root=self.ds_path,
             transform=transforms.Compose(
                 [
-                    transforms.Resize((image_size, image_size)),
-                    transforms.CenterCrop(image_size),
+                    transforms.Resize((self.image_size, self.image_size)),
+                    transforms.CenterCrop(self.image_size),
                     transforms.ToTensor(),
                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                 ]
             ),
         )
-        dog_index = self.dataset.class_to_idx["dog"]
-        cat_index = self.dataset.class_to_idx["cat"]
-        self.dog_dataset = torch.utils.data.Subset(
-            self.dataset, [i for i, t in enumerate(self.dataset.targets) if t == dog_index]
-        )
-        self.cat_dataset = torch.utils.data.Subset(
-            self.dataset, [i for i, t in enumerate(self.dataset.targets) if t == cat_index]
-        )
 
-    def get_dl(self, batch_size=32, shuffle=True, drop_last=True, dog=True):
-        data_set_type = self.dog_dataset if dog else self.cat_dataset
+        if self.animal:
+            dog_index = self.dataset.class_to_idx["dog"]
+            cat_index = self.dataset.class_to_idx["cat"]
+            self.dog_dataset = torch.utils.data.Subset(
+                self.dataset, [i for i, t in enumerate(self.dataset.targets) if t == dog_index]
+            )
+            self.cat_dataset = torch.utils.data.Subset(
+                self.dataset, [i for i, t in enumerate(self.dataset.targets) if t == cat_index]
+            )
+
+    def get_dl(self, batch_size=32, shuffle=True, drop_last=True, ds_type="cat"):
+        if ds_type in ["cat", "dog"]:
+            data_set_type = self.dog_dataset if ds_type == "dog" else self.cat_dataset
+        else:
+            data_set_type = self.dataset
         return torch.utils.data.DataLoader(data_set_type, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last)
 
-    def view_training_data(self):
+    def view_training_data(self, ds_type):
         """
         View training data
         Function that will show a batch of training images
         Utalises Matplotlib to show the images and its animation features.
         """
         # Plot some training images
-        real_batch = next(iter(self.get_dl()))
+        real_batch = next(iter(self.get_dl(ds_type=ds_type)))
 
         plt.figure(figsize=(8, 8))
         plt.axis("off")
@@ -92,8 +100,8 @@ class Data(data.Dataset):
 
         fig = plt.figure()
         ims = []
-        for i in range(10):
-            real_batch = next(iter(self.get_dl()))
+        for _ in range(10):
+            real_batch = next(iter(self.get_dl(ds_type=ds_type)))
             ims.append(
                 [
                     plt.imshow(
@@ -106,5 +114,17 @@ class Data(data.Dataset):
                 ]
             )
         ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
-        ani.save(f"{path}/dynamic_images.mp4")
+        ani.save(f"{self.path}/dynamic_images{ds_type}.mp4")
         plt.show()
+
+    def __call__(self, ds_type, view_train=False):
+        if ds_type in ["cat", "dog"]:
+            self.set_animal_path()
+        else:
+            self.set_human_path()
+        self.create_data()
+        if view_train:
+            self.view_training_data(ds_type)
+
+
+# test = Data()(ds_type="human", view_train=True)
