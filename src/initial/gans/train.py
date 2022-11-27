04eba3c74aee14a96ac655ee9f9bd3172469ba45
vsedov <viv.sedov@hotmail.com>
2022-11-27 21:56:55 +0000

    fix: Use Wandb config, to initialise project- extending dictionaries from params : allows these two models to be more modular.

--- src/initial/gans/train.py
+++ src/initial/gans/train.py
@@ -36,7 +36,7 @@ def weights_init(m):
     elif classname.find("BatchNorm") != -1:
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
-    wandb.watch(m)
+    # wandb.watch(m)
 
 
 def create_checkpoint(
@@ -70,6 +70,7 @@ def create_checkpoint(
 def pickle_dumb(data, path, name):
     with open(f"{path}/{name}.pkl", "wb") as f:
         pickle.dump(data, f)
+        # wandb.save(f"{path}/{name}.pkl")
 
 
 def visualise_training_losss(data_container):
@@ -93,20 +94,36 @@ def visualise(d_container):
 
 
 def main():
-    name_space = "dcgan1"  #  TODO: (vsedov) (17:59:17 - 25/11/22): CHANGE THIS
+    name_space = "dcgan_human_1000"  #  TODO: (vsedov) (17:59:17 - 25/11/22): CHANGE THIS
     learning_init_setup()
 
     check_point_path = f"{os.path.dirname(os.path.abspath(__file__))}/checkpoints"
-
-    data = Data()
-    data.create_data()
     params = hc.initial["params"]
     device = hc.DEFAULT_DEVICE
-    epoch_amount = [2, 4, 100, 300, 500, 1000][2]
+
+    #  REVISIT: (vsedov) (04:30:31 - 26/11/22): Use params dict -> and then add extra information here if required
+    wandb.init(
+        project="dcgan",
+        entity="vsedov",
+        # fmt: off
+        config={**params, **{
+            "test_mode":False ,
+            "checkpoint_path": check_point_path,
+            "ds_type": "human",
+        }},  # cat | dog
+        name=name_space,
+    )
+    config = wandb.config
+    # fmt: on
+
+    data = Data()
+    data(ds_type=config.ds_type, view_train=False)
+    dataloader = data.get_dl(config.batch_size, ds_type=config.ds_type)  # try cat data
+    # print(dataloader)
 
     dis, gen = hp.to_default_device(
-        Discriminator(params["discriminator"], params["output"]),
-        Generator(params["latent_vector"], params["generator"], params["output"]),
+        Discriminator(config.image_size, config.output),
+        Generator(config.latent_vector, config.image_size, config.output),
     )
     print(gen.device)
     print(dis.device)
@@ -114,36 +131,21 @@ def main():
     dis.apply(weights_init)
     gen.apply(weights_init)
 
-    fixed_noise = torch.randn(64, params["latent_vector"], 1, 1, device=device)
+    # Does this need to be Image size and then params["the latent vector"]
+    #
+    fixed_noise = torch.randn(config.noise_vector, config.latent_vector, 1, 1, device=device)
+    # fixed_noise = torch.randn(64, params["latent_vector"], 1, 1, device=device)
 
-    # Create mini batch data
-    batch_size = [32, 64, 128][2]
-    dataloader = data.get_dl(batch_size)
-    learning_rate = 0.0001
-    criterion = nn.BCELoss()  # Binary cross entropy Loss function
-
-    #  TODO: (vsedov) (19:38:28 - 24/11/22): Refactor this into a more subtle method
-
-    optimizer_d = optim.Adam(dis.parameters(), lr=learning_rate, betas=(0.5, 0.999))
-    optimizer_g = optim.Adam(gen.parameters(), lr=learning_rate, betas=(0.5, 0.999))
-
-    wandb.init(
-        project="dcgan",
-        entity="vsedov",
-        config={"epochs": epoch_amount, "batch_size": batch_size, "lr": 0.0001, "model": "dcgan", "test_mode": False},
-        name=name_space,
-    )
+    criterion = nn.BCELoss()  # Binary Cross Entropy Loss used for the discriminator
 
     d_container = train(
         dataloader,
         dis,
         gen,
         criterion,
-        optimizer_d,
-        optimizer_g,
         fixed_noise,
         device,
-        params,
+        config,
         check_point_path,
     )
 
@@ -161,26 +163,28 @@ def train(
     dis,
     gen,
     criterion,
-    optimizer_d,
-    optimizer_g,
     fixed_noise,
     device,
-    params,
+    config,
     check_point_path,
-    name_space,
 ):
     data_container = {"img_list": [], "g_loss": [], "d_loss": [], "iters": 0}
-    batch_size = wandb.config.batch_size
-    epoch_amount = wandb.config.epochs
-    test_mode = wandb.config.test_mode
+    batch_size = config.batch_size
+    epoch_amount = config.epoch_amount
+    test_mode = config.test_mode
     name_space = wandb.run.name
     wandb.watch(gen, log="all")
     wandb.watch(dis, log="all")
+
+    optimizer_d = optim.Adam(dis.parameters(), lr=config.lr + 0.0001, betas=(0.5, 0.999))
+    optimizer_g = optim.Adam(gen.parameters(), lr=config.lr, betas=(0.5, 0.999))
+    print("Starting Training Loop...")
+
     # Log original images
 
     for epoch in tqdm.tqdm(range(epoch_amount)):
 
-        for i, batch in tqdm.tqdm(enumerate(dataloader), total=len(dataloader)):
+        for i, batch in enumerate(dataloader):
             dis.train()
             gen.train()
             X, _ = batch
@@ -195,7 +199,7 @@ def train(
             #  ──────────────────────────────────────────────────────────────────────
             #
             # Generator time
-            noise = torch.randn(batch_size, params["latent_vector"], 1, 1, device=device)
+            noise = torch.randn(batch_size, config.latent_vector, 1, 1, device=device)
             fake = gen(noise)
             label.fill_(0.0)
             output = dis(fake.detach()).view(-1)
@@ -215,17 +219,13 @@ def train(
             derivative_g = output.mean().item()
             optimizer_g.step()
 
-            if i % 10 == 0:
+            if i % epoch_amount / 10 == 0:
+
                 print(
                     f"Epoch [{epoch}/{epoch_amount}] Batch {i}/{len(dataloader)} "
                     f"Loss D: {error_d.item():.4f} Loss G: {error_g.item():.4f} "
-                    f"D(x): {derivative_d_real:.4f} D(G(z)): {derivative_d_fake:.4f} / {derivative_g:.4f}"
+                    f"D(x): {derivative_d_real:.4f} D(G(z)): {derivative_d_fake:.4f} / {derivative_g:.4f} "
                 )
-
-            #  ──────────────────────────────────────────────────────────────────────
-            # Checkpoints are always nice to have
-            if i % 200 == 0:
-                create_checkpoint(dis, gen, optimizer_d, optimizer_g, epoch, check_point_path, name_space, test_mode)
 
             data_container["g_loss"].append(error_g.item())
             data_container["d_loss"].append(error_d.item())
@@ -233,6 +233,12 @@ def train(
 
             dis.eval()
             gen.eval()
+
+        # Compare which model is better
+        if (derivative_d_real > derivative_d_fake) and (derivative_d_real > derivative_g):
+            print("Discriminator is better")
+        elif (derivative_d_fake > derivative_d_real) and (derivative_d_fake > derivative_g):
+            print("Generator is better")
 
         wandb.log(
             {
@@ -242,16 +248,33 @@ def train(
                 "Discriminator Fake": derivative_d_fake,
                 "Generator": derivative_g,
                 "Epoch": epoch,
+                "progress": epoch / epoch_amount,
+                "Better Model": "Discriminator"
+                if (derivative_d_real > derivative_d_fake) and (derivative_d_real > derivative_g)
+                else "Generator"
+                if (derivative_d_fake > derivative_d_real) and (derivative_d_fake > derivative_g)
+                else "None",
             }
         )
-        # validation
 
         with torch.no_grad():
             fake = gen(fixed_noise).detach().cpu()
 
         data_container["img_list"].append(vutils.make_grid(fake, padding=2, normalize=True))
-        wandb.log({"Generated Images": [wandb.Image(i) for i in data_container["img_list"]]})
-        wandb.log({"Original Images": [wandb.Image(i) for i in X]})
+
+        if epoch % (epoch_amount / 4) == 0:
+            print("Saving Images")
+            wandb.log({"Generated Images": [wandb.Image(i) for i in data_container["img_list"]]})
+            wandb.log({"Original Images": [wandb.Image(i) for i in X]})
+
+            data_table = wandb.Table(columns=["Original Images", "Generated Images"])
+            data_table.add_data(wandb.Image(X[0]), wandb.Image(fake[0]))
+            wandb.log({"Image Comparison": data_table})
+
+            create_checkpoint(dis, gen, optimizer_d, optimizer_g, epoch, check_point_path, name_space, test_mode)
+
+        # log weights and bias
+        # wandb.log({"weights": wandb.Histogram(dis.parameters().grad)})
 
     # Wandb save model
     wandb.save(f"{check_point_path}/{name_space}_dis.pth")
