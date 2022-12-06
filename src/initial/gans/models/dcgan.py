ead4e50301634a1246748dcba6c1b39fe38071b4
vsedov <viv.sedov@hotmail.com>
2022-12-06 08:34:05 +0000

    remove time line folder

--- src/initial/gans/models/dcgan.py
+++ src/initial/gans/models/dcgan.py
@@ -5,6 +5,7 @@ from src.core import hc, hp
 
 
 class Generator(nn.Module):
+
     def __init__(self, n_z, n_f, n_c, relu_slope=0.2):
         """Generator Init
         n_z : Latent Vector Size
@@ -23,13 +24,11 @@ class Generator(nn.Module):
             nn.BatchNorm2d(n_f * 8),
             nn.LeakyReLU(negative_slope=relu_slope, inplace=True),
             nn.ConvTranspose2d(
-                in_channels=n_f * 8, out_channels=n_f * 4, kernel_size=4, stride=2, padding=1, bias=False
-            ),
+                in_channels=n_f * 8, out_channels=n_f * 4, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(n_f * 4),
             nn.LeakyReLU(negative_slope=relu_slope, inplace=True),
             nn.ConvTranspose2d(
-                in_channels=n_f * 4, out_channels=n_f * 2, kernel_size=4, stride=2, padding=1, bias=False
-            ),
+                in_channels=n_f * 4, out_channels=n_f * 2, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(n_f * 2),
             nn.LeakyReLU(negative_slope=relu_slope, inplace=True),
             nn.ConvTranspose2d(in_channels=n_f * 2, out_channels=n_f, kernel_size=4, stride=2, padding=1, bias=False),
@@ -48,6 +47,7 @@ class Generator(nn.Module):
 
 
 class Discriminator(nn.Module):
+
     def __init__(self, n_f, n_c, relu_slope=0.2):
         super().__init__()
         self.discriminator = nn.Sequential(
@@ -79,7 +79,7 @@ def show_info():
     params = hc.initial["params"]
 
     gen = Generator(params["latent_vector"], params["image_size"], params["output"])
-    dis = Discriminator(params["discriminator"], params["output"])
+    dis = Discriminator(params["image_size"], params["output"])
 
     hp.sum(gen)
     hp.sum(dis)
