3b1f9e73af7f64db1a1b5fb7d002809f9de9dc2d
vsedov <viv.sedov@hotmail.com>
2023-03-11 09:59:56 +0000

    Refactor: create_data: folder now contains core data to create the dataset

---
+++ src/create_data/gen_gif.py
@@ -0,0 +1,29 @@
+import argparse
+import glob
+import os
+
+import imageio
+
+parser = argparse.ArgumentParser(
+    description="Create a gif from a directory of images"
+)
+parser.add_argument(
+    "--save_path",
+    default="../../results/anime.gif",
+    help="Path to save the gif",
+)
+parser.add_argument(
+    "--img_dir",
+    default="../../results/",
+    help="Directory containing the images",
+)
+parser.add_argument(
+    "--max_frames", type=int, default=1, help="Max number of frames in the gif"
+)
+args = parser.parse_args()
+
+filenames = sorted(glob.glob(os.path.join(args.img_dir, "*.jpg")))
+step = len(filenames) // args.max_frames if args.max_frames else 1
+images = (imageio.imread(filename) for filename in filenames[::step])
+
+imageio.mimsave(args.save_path, images, fps=8)
