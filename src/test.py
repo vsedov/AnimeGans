9f76aa653786a3a3ff5e7871f8e7a94311ea76a1
vsedov <viv.sedov@hotmail.com>
2023-03-11 15:31:36 +0000

    Feat: Create image loader / tester allow the user to generate images

--- src/test.py
+++ src/test.py
@@ -87,3 +87,67 @@ def parse_args():
     args.gen_model_dir = f"{current_path}/results/checkpoints/ACGAN-[{args.batch_size}]-[{args.epoch}]/G_{args.check_point_number}.ckpt"
     return Args(*args.__dict__.values())
 
+
+def main(args):
+    """
+    Main function.
+
+    Args:
+        args: Arguments.
+
+    Notes:
+        - The function will generate anime characters based on the given arguments.
+        Generated samples will be saved in the folder specified by the argument `sample_dir`.
+        - The function will generate anime characters based on the given arguments.
+
+    Returns:
+        None
+
+    """
+    os.makedirs(args.sample_dir, exist_ok=True)
+    device = "cpu" if not torch.cuda.is_available() else "cuda"
+    latent_dim = 128
+    hair_classes = len(hair_mapping)
+    eye_classes = len(eye_mapping)
+
+    G = Generator(latent_dim, hair_classes + eye_classes).to(device)
+    prev_state = torch.load(args.gen_model_dir)
+    G.load_state_dict(prev_state["model"])
+    G = G.eval()
+
+    action_map = {
+        "fix_hair_eye": lambda: generate_by_attributes(
+            G,
+            device,
+            latent_dim,
+            hair_classes,
+            eye_classes,
+            args.sample_dir,
+            hair_color=args.hair,
+            eye_color=args.eye,
+        ),
+        "change_eye": lambda: eye_grad(
+            G, device, latent_dim, hair_classes, eye_classes, args.sample_dir
+        ),
+        "change_hair": lambda: hair_grad(
+            G, device, latent_dim, hair_classes, eye_classes, args.sample_dir
+        ),
+        "interpolate": lambda: interpolate(
+            G, device, latent_dim, hair_classes, eye_classes, args.sample_dir
+        ),
+        "fix_noise": lambda: fix_noise(
+            G, device, latent_dim, hair_classes, eye_classes, args.sample_dir
+        ),
+    }
+
+    action_map.get(args.type, lambda: None)()
+
+
+def run():
+    parse = parse_args()
+    print("Help")
+    data = parse
+    for key in data._fields:
+        print(key)
+    print("Done")
+    main(parse)
