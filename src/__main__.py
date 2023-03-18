e17d78a26354e0d0cdbee21f1eaf55b733ee569a
vsedov <viv.sedov@hotmail.com>
2023-03-18 06:44:11 +0000

    Fix: __main__.py, allow user to parse a gui as well if flage is called. - also allow user to parse in inputs based on filetype.

--- src/__main__.py
+++ src/__main__.py
@@ -3,24 +3,52 @@ import sys
 
 
 def main():
+    # create the ArgumentParser object
     parser = argparse.ArgumentParser(
         description="Run either train.py or test.py"
     )
+
     parser.add_argument(
-        "mode", type=str, choices=["train", "test"], help="Mode: train or test"
+        "--gui",
+        default=False,
+        action="store_true",
+        help="If set, the GUI for the mode will also be parsed",
     )
+
+    parser.add_argument(
+        "mode",
+        type=str,
+        default="test",
+        choices=["train", "test"],
+        help="Mode: train or test",
+    )
+
     args, remaining_argv = parser.parse_known_args()
 
-    module_map = {"train": "src.train", "test": "src.test"}
+    if len(sys.argv) == 2:
+        # User only provided mode argument, show help for corresponding module
+        if args.mode == "train":
+            from src.train import run
+        else:
+            from src.test import run
+        run("--help")
+    else:
+        if args.gui:
+            module_map = {
+                "train": "src.gui.train_gui",
+                "test": "src.gui.test_gui",
+            }
+        else:
+            module_map = {"train": "src.train", "test": "src.test"}
 
-    module_name = module_map.get(args.mode)
-    if not module_name:
-        raise Exception(f"Invalid mode: {args.mode}")
+        module_name = module_map.get(args.mode)
+        if not module_name:
+            raise Exception(f"Invalid mode: {args.mode}")
 
-    sys.argv = [sys.argv[0]] + remaining_argv
-    module = __import__(module_name, fromlist=["run"])
-    
-    module.run()
+        sys.argv = [sys.argv[0]] + remaining_argv
+        module = __import__(module_name, fromlist=["run"])
+
+        module.run()
 
 
 if __name__ == "__main__":
